75066270d7d81266f86d3deb21d4e22d6077c9f5
audrikaindu231 <111930284+audrikaindu231@users.noreply.github.com>
2022-10-27 23:16:38 -0400

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setup.py import setup
+
+setup(name='Demo',
+      version='0.1',
+      description='A simple demonstration package',
+      url='https://github.com/audrikaindu231/M09',
+      author='Audrika Indu',
+      author_email='cet6dn@virginia.edu'
+      license='MIT'
+      packages='Module09'
